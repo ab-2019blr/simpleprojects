c173466bcd83a5c01300c992bb72b0d3620ab3b6
ab-2019blr <akashbanerji.ab@gmail.com>
2025-10-21 15:48:00 +0530

    Updated watchlist & removed analytics page

--- StockMarketApp/pages/analytics.py
+++
@@ -1,7 +0,0 @@
-# Analytics and reports page implementation goes here (placeholder) 
-import streamlit as st
-# Set page configuration
-st.set_page_config(page_title="Analytics", layout="centered", page_icon="📈")
-
-st.subheader("Secondary Page: Analytics Page")
-st.write("This is the secondary page of the StockMarketApp app. More content can be added here.")
